e2f71c10fa14f72e9b9369b78f55392d4ae86f7c
madamerobot <hi@valerie-fuchs.de>
2018-04-25 17:10:10 +0200

    Setting up Graphiql integration

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 require('dotenv').config()
-const express = require('express')
-const app = express()
 
-//Pulling in Contentful Access Token
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+//Contentful Access Tokini
 const spaceId = process.env.SPACE_ID
 const cdaToken = process.env.CDA_TOKEN
 const cmaToken = process.env.CMA_TOKEN
 const port = process.env.PORT
 
 const cfGraphql = require('cf-graphql')
-//Setting up Express/GraphQL Server
 const graphqlHTTP = require('express-graphql')
 
-//====Create a GraphQL Schema=====//
+//====Creating a GraphQL Schema based on CF Content Models=====//
 
-//Creating a new Contentful Client
-
+//Initialising new Contentful & GraphQL  Client
 const client = cfGraphql.createClient( {spaceId, cdaToken, cmaToken} )
 
-//Fetching Content Model from CF
+//Fetching Content Model from Contentful
 client.getContentTypes()
-//Creating GraphQL Model based on it
+//Creating GraphQL Schema based on it
 .then(cfGraphql.prepareSpaceGraph)
 .then(spaceGraph => {
   const names = spaceGraph.map(ct => ct.names.type).join(', ')
@@ -28,21 +28,37 @@ client.getContentTypes()
   return spaceGraph
 })
 .then(cfGraphql.createSchema)
-//Start Server which uses GpraphQL model, connected to 
-//CF client
+//Start Server which uses GraphQL Schema
 .then(schema => startServer(client, schema))
 
 
-//====Spin up GraphQL Server====//
+//====Spinning up GraphQL Server====//
 
 function startServer (client, schema) {
 
-  app.use('/graphql', graphqlHTTP({
-    context: {entryLoader: client.createEntryLoader()},
-    graphql: true,
-    schema,
-  }))
+  const app = express()
+  app.use(cors())
+
+  // serve React Frontend
+  app.use('/client', express.static(path.join(__dirname, 'dist')))
+
+  // ship own GraphiQL to be completely flexible
+  const ui = cfGraphql.helpers.graphiql({title: 'datafriends'})
+  app.get('/', (_, res) => res.set(ui.headers).status(ui.statusCode).end(ui.body))
+
+  // enable GraphQL extension to retrieve GraphQL versoin and
+  // detailed HTTP request information
+  const opts = {version: true, timeline: true, detailedErrors: false}
+  const ext = cfGraphql.helpers.expressGraphqlExtension(client, schema, opts)
+  app.use('/graphql', graphqlHTTP(ext))
+
+  // app.use('/graphql', graphqlHTTP({
+  //   context: {entryLoader: client.createEntryLoader()},
+  //   graphql: true,
+  //   schema,
+  // }))
 
   app.listen(port)
+
   console.log(`🌟 Running a GraphQL server, listening on port ${port}🌟`)
 }
